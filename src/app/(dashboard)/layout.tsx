63dd927debbbe1ce3c471359acee2172dfc418bf
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2025-01-30 07:44:09 +0700

    feat(slicing-ui): add menu and navbar components

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,33 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[10%] lg:min-w-[200px] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/logo.png" alt="Logo" width={32} height={32} />
+          <span className="hidden lg:block">SchooLama</span>
+        </Link>
+
+        <Menu />
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[90%] lg:min-w-[calc(100%-200px)] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
